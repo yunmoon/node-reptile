0b6674098e3470fa43033d1357cbde02a586c47f
meiyun1 <565548320@qq.com>
2018-07-26 14:17:36 +0800

    爬取数据增加跳转链接，时间和标签

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const app = require('express')();
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function getIndexPage () {
-  const str = await axios.get('https://laravel-china.org/topics');
+async function getIndexPage (filter = 'default', page = 1) {
+  const str = await axios.get(`https://laravel-china.org/topics?filter=${filter}&page=${page}`);
   const body = str.data;
   const $ = cheerio.load(body);
   const item = [];
@@ -13,14 +13,17 @@ async function getIndexPage () {
       name: $element.find('.avatar>a').attr('title'),
       avatar: $element.find('.avatar>a>img').attr('src'),
       title: $element.find('.infos>.media-heading>a').text().replace(/ |\n/g, ''),
+      url: $element.find('.infos>.media-heading>a').attr('href'),
+      time: $element.find('.reply_count_area>.count_set>.timeago').text(),
+      label: $element.find('.infos>.media-heading>.label').text(),
     })
   });
   return item;
 }
 
 
-app.use('/', async (req, res) => {
-  const items = await getIndexPage();
+app.get('/topics', async (req, res) => {
+  const items = await getIndexPage(req.query.filter, req.query.page);
   return res.json(items);
 });
 
